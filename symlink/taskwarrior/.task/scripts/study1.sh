375ad59a12c5943c1b98dffe2afc367bb65666b2
BSDxxxx <yaohunzhanyue@qq.com>
2020-05-06 00:12:51 +0800

    update

--- symlink/taskwarrior/.task/scripts/study1.sh
+++ symlink/taskwarrior/.task/scripts/study1.sh
@@ -1,7 +1,20 @@
-str=$(task add project:考研 +高数             <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
-str=$(task add project:考研 +高数 depends:$id <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
-str=$(task add project:考研 +英语 depends:$id <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
-str=$(task add project:考研 +英语 depends:$id <++>)
+# 奖励
+str=$(task add project:考研 +奖励             due:<++> <++>)
+bonous_id=$(echo $str | grep -o -E '[0-9]+')
+
+# 学习内容
+str=$(task add project:考研 +高数             due:<++> <++>)
+id=$(echo $str | grep -o -E '[0-9]+')
+task $bonous_id modify depends:$id
+
+str=$(task add project:考研 +高数 depends:$id due:<++> <++>)
+id=$(echo $str | grep -o -E '[0-9]+')
+task $bonous_id modify depends:$id
+
+str=$(task add project:考研 +英语 depends:$id due:<++> <++>)
+id=$(echo $str | grep -o -E '[0-9]+')
+task $bonous_id modify depends:$id
+
+str=$(task add project:考研 +英语 depends:$id due:<++> <++>)
+id=$(echo $str | grep -o -E '[0-9]+')
+task $bonous_id modify depends:$id
--- symlink/taskwarrior/.task/scripts/study2.sh
+++ symlink/taskwarrior/.task/scripts/study2.sh
@@ -1,7 +1,20 @@
-str=$(task add project:考研 +政治             <++>)
+# 奖励
+str=$(task add project:考研 +奖励             due:<++> <++>)
+bonous_id=$(echo $str | grep -o -E '[0-9]+')
+
+# 学习内容
+str=$(task add project:考研 +政治             due:<++> <++>)
 id =$(echo $str | grep -o -E '[0-9]+')
-str=$(task add project:考研 +政治 depends:$id <++>)
+task $bonous_id modify depends:$id
+
+str=$(task add project:考研 +政治 depends:$id due:<++> <++>)
 id =$(echo $str | grep -o -E '[0-9]+')
-str=$(task add project:考研 +专业 depends:$id <++>)
-id =$(echo $str | grep -o -E '[0-9]+'`
-str=$(task add project:考研 +专业 depends:$id <++>)
+task $bonous_id modify depends:$id
+
+str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
+id =$(echo $str | grep -o -E '[0-9]+')
+task $bonous_id modify depends:$id
+
+str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
+id =$(echo $str | grep -o -E '[0-9]+')
+task $bonous_id modify depends:$id
