d2b732fac23211a9deaebb849699b45279ac4b77
BSDxxxx <yaohunzhanyue@qq.com>
2020-05-09 00:37:21 +0800

    Bug fix

--- symlink/taskwarrior/.task/scripts/study2.sh
+++ symlink/taskwarrior/.task/scripts/study2.sh
@@ -4,17 +4,17 @@ bonous_id=$(echo $str | grep -o -E '[0-9]+')
 
 # 学习内容
 str=$(task add project:考研 +政治             due:<++> <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
+id=$(echo $str | grep -o -E '[0-9]+')
 task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +政治 depends:$id due:<++> <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
+id=$(echo $str | grep -o -E '[0-9]+')
 task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
+id=$(echo $str | grep -o -E '[0-9]+')
 task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
-id =$(echo $str | grep -o -E '[0-9]+')
+id=$(echo $str | grep -o -E '[0-9]+')
 task $bonous_id modify depends:$id
