df45a7378f01c4fe89bf01c28961a4b3b5845b79
root <root@DESKTOP-JHAAF6Q.localdomain>
2020-05-10 11:24:32 +0800

    Elaborate

--- symlink/taskwarrior/.task/scripts/study1.sh
+++ symlink/taskwarrior/.task/scripts/study1.sh
@@ -1,20 +1,16 @@
-# 奖励
-str=$(task add project:考研 +奖励             due:<++> <++>)
-bonous_id=$(echo $str | grep -o -E '[0-9]+')
-
-# 学习内容
 str=$(task add project:考研 +高数             due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +高数 depends:$id due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
+
+# ---------------------------------------------------------
+task add project:筑基 +休息                   due:<++> 眼药水 & 按摩器
+task add project:筑基 +静功                   due:<++> <++>
+# ---------------------------------------------------------
 
 str=$(task add project:考研 +英语 depends:$id due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +英语 depends:$id due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
--- symlink/taskwarrior/.task/scripts/study2.sh
+++ symlink/taskwarrior/.task/scripts/study2.sh
@@ -1,20 +1,16 @@
-# 奖励
-str=$(task add project:考研 +奖励             due:<++> <++>)
-bonous_id=$(echo $str | grep -o -E '[0-9]+')
-
-# 学习内容
 str=$(task add project:考研 +政治             due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +政治 depends:$id due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
+
+# ---------------------------------------------------------
+task add project:筑基 +休息                   due:<++> 眼药水 & 按摩器
+task add project:筑基 +动功                   due:<++> <++>
+# ---------------------------------------------------------
 
 str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
 
 str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
-task $bonous_id modify depends:$id
