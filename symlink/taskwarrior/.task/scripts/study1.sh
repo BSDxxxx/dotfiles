90fb9b64521252d257da62678aacb1da5d90f4f3
root <root@DESKTOP-JHAAF6Q.localdomain>
2020-05-11 09:10:57 +0800

    Update taskwarrior settings and scripts

--- symlink/taskwarrior/.task/scripts/study1.sh
+++ symlink/taskwarrior/.task/scripts/study1.sh
@@ -1,16 +1,16 @@
-str=$(task add project:考研 +高数             due:<++> <++>)
+str=$(task add project:考研 +高数             due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
-str=$(task add project:考研 +高数 depends:$id due:<++> <++>)
+str=$(task add project:考研 +高数 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
 # ---------------------------------------------------------
-task add project:筑基 +休息                   due:<++> 眼药水 & 按摩器
-task add project:筑基 +静功                   due:<++> <++>
+task add project:筑基 +休息                   due:today+2d 眼药水 & 按摩器
+task add project:筑基 +静功                   due:today+2d <++>
 # ---------------------------------------------------------
 
-str=$(task add project:考研 +英语 depends:$id due:<++> <++>)
+str=$(task add project:考研 +英语 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
-str=$(task add project:考研 +英语 depends:$id due:<++> <++>)
+str=$(task add project:考研 +英语 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
--- symlink/taskwarrior/.task/scripts/study2.sh
+++ symlink/taskwarrior/.task/scripts/study2.sh
@@ -1,16 +1,16 @@
-str=$(task add project:考研 +政治             due:<++> <++>)
+str=$(task add project:考研 +政治             due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
-str=$(task add project:考研 +政治 depends:$id due:<++> <++>)
+str=$(task add project:考研 +政治 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
 # ---------------------------------------------------------
-task add project:筑基 +休息                   due:<++> 眼药水 & 按摩器
-task add project:筑基 +动功                   due:<++> <++>
+task add project:筑基 +休息                   due:today+2d 眼药水 & 按摩器
+task add project:筑基 +动功                   due:today+2d <++>
 # ---------------------------------------------------------
 
-str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
+str=$(task add project:考研 +专业 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
-str=$(task add project:考研 +专业 depends:$id due:<++> <++>)
+str=$(task add project:考研 +专业 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
