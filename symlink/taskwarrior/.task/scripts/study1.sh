8e58e47f00820863a3dbb6382a70cf101953b856
root <root@DESKTOP-JHAAF6Q.localdomain>
2020-05-12 10:30:41 +0800

    Escape special character '&'

--- symlink/taskwarrior/.task/scripts/study1.sh
+++ symlink/taskwarrior/.task/scripts/study1.sh
@@ -5,7 +5,7 @@ str=$(task add project:考研 +高数 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
 # ---------------------------------------------------------
-task add project:筑基 +休息                   due:today+2d 眼药水 & 按摩器
+task add project:筑基 +休息                   due:today+2d '眼药水 & 按摩器'
 task add project:筑基 +静功                   due:today+2d <++>
 # ---------------------------------------------------------
 
--- symlink/taskwarrior/.task/scripts/study2.sh
+++ symlink/taskwarrior/.task/scripts/study2.sh
@@ -5,7 +5,7 @@ str=$(task add project:考研 +政治 depends:$id due:today+2d <++>)
 id=$(echo $str | grep -o -E '[0-9]+')
 
 # ---------------------------------------------------------
-task add project:筑基 +休息                   due:today+2d 眼药水 & 按摩器
+task add project:筑基 +休息                   due:today+2d '眼药水 & 按摩器'
 task add project:筑基 +动功                   due:today+2d <++>
 # ---------------------------------------------------------
 
